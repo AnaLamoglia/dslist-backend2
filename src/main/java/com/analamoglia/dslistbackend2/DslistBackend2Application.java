1606906bfa0392eb17c47f803ad10c430e4b090a
AnaLamoglia <150859273+AnaLamoglia@users.noreply.github.com>
2023-11-18 16:57:57 -0300

    Add files via upload

---
+++ src/main/java/com/analamoglia/dslistbackend2/DslistBackend2Application.java
@@ -0,0 +1,13 @@
+package com.analamoglia.dslistbackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistBackend2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/analamoglia/dslistbackend2/DslistBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analamoglia.dslistbackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
